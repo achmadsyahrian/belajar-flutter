704a90156a77db540ee113c8c0057c1a98d72602
Achmad Syahrian <achmadsyahrian1@gmail.com>
2023-12-15 11:35:01 +0700

    Control Text Field

--- lib/main.dart
+++ lib/main.dart
@@ -5,92 +5,59 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController myController = TextEditingController();
+
+  String hasil = "Hasil Input";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "Poppins"),
+      theme: ThemeData(fontFamily: 'Poppins'),
       home: Scaffold(
-        appBar: appBar("Text Field"),
+        appBar: appBar("Control Text Field"),
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(30),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                inputField(),
-                SizedBox(height: 20,),
-                inputField(),
-                SizedBox(height: 20,),
-                inputField(),
+                TextField(
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          myController.text = "";
+                          hasil = "";
+                        });
+                      } , 
+                      icon: Icon(Icons.disabled_by_default),
+                    )
+                  ),
+                  controller: myController,
+                  onChanged: (value) {
+                    setState(() {
+                      hasil = value;
+                    });
+                  } ,
+                  // onSubmitted: (value) {
+                    
+                  // },
+                  // onEditingComplete: () {
+                  //   print("Edit Success");
+                  //   print(myController.text);
+                  // },
+                ),
+                Text(hasil),
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-
-  TextField inputField() {
-    return TextField(
-      // // Fitur
-      // autocorrect: true,
-      // autofocus: true,
-      // maxLength: 20,
-      // enableSuggestions: true,
-      // // enableInteractiveSelection: false,
-      // enabled: true,
-      // // obscureText: true, //password
-      // // obscuringCharacter: "*", //custom password
-      // keyboardType: TextInputType.number,
-      // cursorColor: Colors.black,
-      // textAlign: TextAlign.right,
-      // // readOnly: true,
-      showCursor: true,
-      // cursorWidth: 10,
-      // cursorRadius: Radius.circular(10),
-      textAlignVertical: TextAlignVertical.center,
-      textCapitalization: TextCapitalization.words,
-      style: TextStyle(
-        color: Color.fromARGB(221, 81, 79, 224),
-      ),
-      decoration: InputDecoration(
-        prefixIcon: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Icon(Icons.person),
-        ),
-        suffixIcon: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Icon(Icons.library_add_check_rounded),
-        ),
-        prefixIconColor: Color.fromARGB(221, 81, 79, 224),
-        suffixIconColor: Color.fromARGB(221, 81, 79, 224),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(
-            color: Colors.red
-          )
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(
-            color: Color.fromARGB(221, 81, 79, 224),
-            width: 2
-          )
-        ),
-        hintText: "Achmad Syahrian", //place holder
-        hintStyle: TextStyle(
-          fontSize: 14,
-          color: Color.fromARGB(190, 172, 172, 172)
-        ),
-        labelText: "Name",
-        labelStyle: TextStyle(
-          color: Color.fromARGB(221, 81, 79, 224),
-        ),
-        floatingLabelStyle: TextStyle(
-          color: Colors.black
         ),
       ),
     );
