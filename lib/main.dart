fb6983bdd20c36dac9690dc1d3f7137ba31f1aaf
Achmad Syahrian <achmadsyahrian1@gmail.com>
2023-12-19 13:43:59 +0700

    Switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:first_project/pages/route/gallery_page.dart';
-import 'package:first_project/pages/route/home_page.dart';
-import 'package:first_project/pages/route/photo_page.dart';
 import 'package:first_project/partials/appbar.dart';
 import 'package:flutter/material.dart';
 
@@ -13,89 +10,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-      routes: {
-        HomePage.routeName : (context) => HomePage(),
-        GalleryPage.routeName : (context) => GalleryPage(),
-        PhotoPage.routeName : (context) => PhotoPage(),
-      },
+      home: MyHome(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
 
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  bool statusSwitch = false;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar("Drawer"),
-      drawer: Drawer(
-        backgroundColor: Color.fromARGB(255, 81, 79, 224),
-        width: 250,
+      appBar: appBar("Switch"),
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: double.infinity,
-              height: 150,
-              // color: Color.fromARGB(255, 41, 37, 235),
-              padding: EdgeInsets.only(left: 20),
-              alignment: Alignment.centerLeft,
-              child: Row(
-                children: [
-                  Icon(Icons.menu_book, size: 35, color: Colors.white,),
-                  SizedBox(width: 7,),
-                  Text("My Menu", 
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontFamily: 'Poppins'
-                    ),
-                  ),
-                ],
-              ),
+            Switch(
+              activeColor: Color.fromARGB(255, 81, 79, 224),
+              activeThumbImage: AssetImage('images/mypict.jpg'),
+              inactiveThumbImage: AssetImage('images/flower.jpg'),
+              value: statusSwitch,
+              onChanged: (value) {
+                setState(() {
+                  statusSwitch = !statusSwitch;
+                });
+              },
             ),
-            SizedBox(
-              height: 10,
-            ),
-            ListTile(
-              onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
-              leading: Icon(Icons.home, color: Colors.white,),
-              title: Text("Home", 
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Text(
+                (!statusSwitch) ? "Switch Off" : "Switch On", 
                 style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Poppins'
-                ),
-              ),
-            ),
-            ListTile(
-              onTap: () => Navigator.pushReplacementNamed(context, GalleryPage.routeName),
-              leading: Icon(Icons.photo_library_rounded, color: Colors.white,),
-              title: Text("Gallery", 
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Poppins'
-                ),
-              ),
-            ),
-            ListTile(
-              onTap: () => Navigator.pushReplacementNamed(context, PhotoPage.routeName),
-              leading: Icon(Icons.photo_camera, color: Colors.white,),
-              title: Text("Photo", 
-                style: TextStyle(
-                  color: Colors.white,
+                  fontSize: 20,
                   fontFamily: 'Poppins'
                 ),
               ),
             )
           ],
-        ),
-      ),
-      body: Center(
-        child: Text("Hello Prof. Achmad", 
-          style: TextStyle(
-            fontSize: 20,
-            fontFamily: 'Poppins'
-          ),
         ),
       ),
     );
